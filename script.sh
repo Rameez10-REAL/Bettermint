7434ecc26347aea395db7d6960302b339e5e4c45
Rameez10 <Rameezmajid10@outlook.com>
2022-07-10 09:00:41 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,16 @@ sudo add-apt-repository ppa:lutris-team/lutris -y
 sudo apt update -y
 sudo apt remove hypnotix -y
 sudo apt remove warpinator -y
+sudo apt remove vlc
+#Photogimp Installer
+flatpak install flathub org.gimp.GIMP
+flatpak run org.gimp.GIMP
+#Work out how to kill GIMP
+xkill
+sudo apt install git -y
+mkdir DELETETHISFOLDER
+git clone https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip DELETETHISFOLDER
+tar -C $HOME -h -xzf PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
 #If you are having issues with audio lagging/stuttering https://forums.linuxmint.com/viewtopic.php?t=326737 or https://forums.linuxmint.com/viewtopic.php?t=245160
 #Fish Installer
 sudo add-apt-repository ppa:fish-shell/release-3 -y
